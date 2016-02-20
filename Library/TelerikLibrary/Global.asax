5343c882d6ddc89c79d0afb76281c7ec1ccf7d56
Samborskiy Sergey <serg28786@gmail.com>
2016-02-20 21:31:31 +0300

    Initial commit

---
+++ Library/TelerikLibrary/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TelerikLibrary.MvcApplication" Language="C#" %>
---
+++ Library/TelerikLibraryTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TelerikLibraryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
